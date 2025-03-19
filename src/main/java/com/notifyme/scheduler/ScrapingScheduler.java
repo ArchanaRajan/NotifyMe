925029b5c5f16ba83a86582dddff2bc223ad0e50
ArchanaRajan <archana19rajan@gmail.com>
2025-03-19 21:53:34 +0530

    ScrapingScheduler added to run BMS web scraping periodically.

---
+++ src/main/java/com/notifyme/scheduler/ScrapingScheduler.java
@@ -0,0 +1,47 @@
+package com.notifyme.scheduler;
+
+import com.notifyme.service.scraper.BookMyShowScraperService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+@Component
+public class ScrapingScheduler {
+
+    @Autowired
+    private BookMyShowScraperService bookMyShowScraperService;
+
+    // List of movies to monitor
+    private final List<String> moviesToMonitor = Arrays.asList(
+        "Interstellar",
+        "Dragon"
+    );
+
+    // List of locations to monitor
+    private final List<String> locationsToMonitor = Arrays.asList(
+        "Chennai",
+        "Bangalore"
+    );
+
+    @Scheduled(cron = "0 */15 * * * *") // Run every 15 minutes
+    public void scrapeBookMyShow() {
+        log.info("Starting BookMyShow scraping job");
+        for (String movie : moviesToMonitor) {
+            for (String location : locationsToMonitor) {
+                try {
+                    bookMyShowScraperService.scrapeMovieShows(movie, location);
+                    // Add delay between requests to avoid rate limiting
+                    Thread.sleep(5000);
+                } catch (Exception e) {
+                    log.error("Error scraping {} in {}: {}", movie, location, e.getMessage());
+                }
+            }
+        }
+        log.info("Completed BookMyShow scraping job");
+    }
+} 
