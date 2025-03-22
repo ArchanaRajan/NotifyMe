80aab18cd59428f5f574062da253b2da32ffba97
ArchanaRajan <archana19rajan@gmail.com>
2025-03-22 23:48:51 +0530

    Added few headers in JSoup to scrape data from BMS. cron job timing changed to every 10 minutes.

--- src/main/java/com/notifyme/service/scraper/BookMyShowScraperService.java
+++ src/main/java/com/notifyme/service/scraper/BookMyShowScraperService.java
@@ -32,6 +32,10 @@ public class BookMyShowScraperService implements WebScraperService {
             String searchUrl = String.format(SEARCH_URL, movieName.replace(" ", "+"));
             Document doc = Jsoup.connect(searchUrl)
                     .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+                    .header("Accept-Language", "en-US,en;q=0.9")
+                    .header("Accept-Encoding", "gzip, deflate, br")
+                    .header("Connection", "keep-alive")
+                    .timeout(10000)
                     .get();
 
             Elements movieElements = doc.select(".movie-card");
