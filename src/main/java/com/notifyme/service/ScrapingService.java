9dc62ebe267de7e38ac63990adb584b938402769
ArchanaRajan <archana19rajan@gmail.com>
2025-03-26 01:49:25 +0530

    Deleted unused files.

--- src/main/java/com/notifyme/service/ScrapingService.java
+++
@@ -1,8 +0,0 @@
-package com.notifyme.service;
-
-import com.notifyme.model.MovieShow;
-import java.util.List;
-
-public interface ScrapingService {
-    List<MovieShow> scrapeBookMyShowMovies(String movieName, String location);
-}
